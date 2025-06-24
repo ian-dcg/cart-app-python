ee565b966cf9db1e8f648b0cfa78d8e73fda6a26
Ian da Costa <ianndcg@gmail.com>
2025-06-24 19:25:01 -0300

    feat(product): adiciona campo 'setor' aos modelos de entrada e saída

--- app/domains/product/model.py
+++ app/domains/product/model.py
@@ -6,13 +6,16 @@ class ProductInDB(BaseModel):
     name: str
     price: float
     quantity: int
+    setor: str
 
 class ProductCreate(BaseModel):
     name: str
     price: float
     quantity: int
+    setor: str
 
 class ProductUpdate(BaseModel):
     name: Optional[str] = None
     price: Optional[float] = None
     quantity: Optional[int] = None
+    setor: Optional[str] = None
